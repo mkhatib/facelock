63ec425990378929fadab32dbeed11e4ac920dee
mkhatib727 <mkhatib727@06effd6c-c406-11dd-ad05-db3d840e29cc>
2008-12-07 13:57:34 +0000

    Building Very Basic Interface For the Project with a Toolbar and a JList for the Contact List with Icons besides them

---
+++ ContactListCellRenderer.java
@@ -0,0 +1,46 @@
+//package .Users.mkhatib.Desktop.Network-Project.facelock;
+
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.*;
+
+/**
+ * <<Class summary>>
+ *
+ * @author Mohammad Khatib &lt;&gt;
+ * @version $Rev$
+ */
+public class ContactListCellRenderer implements ListCellRenderer {
+    private JLabel label = new JLabel(" ",JLabel.LEFT);
+	private Border lineBorder = BorderFactory.createLineBorder(Color.BLACK, 1);
+	private Border emptyBorder = BorderFactory.createEmptyBorder(2,2,2,2);
+
+	public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus){
+		Object[] pair = (Object[])value;
+		label.setOpaque(true);
+		label.setIcon((ImageIcon) pair[0]);
+		label.setText(pair[1].toString());
+		
+		if(isSelected){
+			label.setForeground(list.getSelectionForeground());
+			label.setBackground(list.getSelectionBackground());
+		}
+		else {
+			label.setForeground(list.getForeground());
+			label.setBackground(list.getBackground());
+		}
+		
+		label.setBorder(cellHasFocus ? lineBorder : emptyBorder);
+		
+		return label;
+	}
+	
+	// {{{ ContactList constructor
+    /**
+     * 
+     */
+    public ContactListCellRenderer() {
+        
+    }
+	// }}}
+}
