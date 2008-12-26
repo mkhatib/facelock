829db4aaeb34507173117896b8837b79ba25fa63
mkhatib727 <mkhatib727@06effd6c-c406-11dd-ad05-db3d840e29cc>
2008-12-26 16:06:15 +0000

    Added FLServer.java and FLClient.java

---
+++ trunk/FLClient.java
@@ -0,0 +1,18 @@
+/**
+ * <<Class summary>>
+ *
+ * @author Mohammad Khatib &lt;&gt;
+ * @version $Rev$
+ */
+public final class FLClient {
+    private String IP = "localhost";
+	private int port = 7488;
+	// {{{ FLClient constructor
+    /**
+     * 
+     */
+    public FLClient() {
+        
+    }
+	// }}}
+}
